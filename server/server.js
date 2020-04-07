bf2a3229f23f0f2309076d364190c76008e1cb4d
CJ Weaver <cdw2014@gmail.com>
2020-04-06 21:42:29 -0400

    fix

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ mongoose
 	.catch((err) => console.error(err));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../src/build'));
+	app.use(express.static(path.join(__dirname, '../src/build')));
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'src', 'build', 'index.html'));
 	});
