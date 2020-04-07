fe107507a261a1576b88093b99fb03d31a3b7e6a
CJ Weaver <cdw2014@gmail.com>
2020-04-06 20:28:46 -0400

    build

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api/suggestions', suggestionRoutes);
 app.use('/api/boards', boardRoutes);
 
